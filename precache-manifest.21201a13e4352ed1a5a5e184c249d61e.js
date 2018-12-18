f2b2eaf3e9ba5450a20cb9ee82ad6d82c7fe2214
furball018 <margonbra@gmail.com>
2018-12-17 21:00:26 -0300

    Updates

---
+++ precache-manifest.21201a13e4352ed1a5a5e184c249d61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "448cd0807a63b1a6323b",
+    "url": "/hagamos-numeros-react/static/js/runtime~main.448cd080.js"
+  },
+  {
+    "revision": "c65b5aac9784b53d629d",
+    "url": "/hagamos-numeros-react/static/js/main.c65b5aac.chunk.js"
+  },
+  {
+    "revision": "d30c8f3372b7e0987115",
+    "url": "/hagamos-numeros-react/static/js/1.d30c8f33.chunk.js"
+  },
+  {
+    "revision": "c65b5aac9784b53d629d",
+    "url": "/hagamos-numeros-react/static/css/main.9d9617b6.chunk.css"
+  },
+  {
+    "revision": "d30c8f3372b7e0987115",
+    "url": "/hagamos-numeros-react/static/css/1.16b6c78b.chunk.css"
+  },
+  {
+    "revision": "e34da9c0abe24ab47db580504202759d",
+    "url": "/hagamos-numeros-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hagamos-numeros-react/build/precache-manifest.51c1a454ce67dfc8d7d36cab6f965a30.js"
+  "/hagamos-numeros-react/precache-manifest.21201a13e4352ed1a5a5e184c249d61e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hagamos-numeros-react/build/index.html", {
+workbox.routing.registerNavigationRoute("/hagamos-numeros-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
